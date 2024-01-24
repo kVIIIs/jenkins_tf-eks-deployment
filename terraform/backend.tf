fb340660a4c975593025ef6598667946c77f4cf3
nykzaR <nykzar@outlook.com>
2024-01-25 01:27:17 +0530

    added changes

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "hypha-k8s-project"
     region = "ap-south-2"
-    key = "deployment/terraform.tfstate"
+    key = "deployment/terraform.tfstate"   # Create a "deployment.pem" key pair from aws and save it to jenkins server from where the Jenkinsfile is executed. 
   }
 }
